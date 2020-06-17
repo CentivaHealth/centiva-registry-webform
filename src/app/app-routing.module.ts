aa4fd507993a7fa22c5afeb8d0321a219971db18
PavloTurchyn <pavlo.t@centiva.health>
2020-06-17 20:04:41 +0300

    added auth service + login logic + logout logic

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from '@pages/login/login/login.component';
+import { FormPageComponent } from '@pages/form-page/form-page.component';
 
 const routes: Routes = [
 	{ path: '', component: LoginComponent },
-	{ path: 'login', component: LoginComponent }
+	{ path: 'login', component: LoginComponent },
+	{ path: 'form', component: FormPageComponent }
 ];
 
 @NgModule({
