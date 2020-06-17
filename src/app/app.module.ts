a6ede7d441b7ebe52f3aab49efa08146b8342947
PavloTurchyn <pavlo.t@centiva.health>
2020-06-17 17:17:28 +0300

    added auth wrapper and core,libs module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { PagesModule } from '@pages/pages.module';
-import { AngularFireModule } from '@angular/fire';
-import { AngularFireAuthModule } from '@angular/fire/auth';
-
+import { CoreModule } from '@core/core.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { environment } from '../environments/environment';
 
 @NgModule({
 	declarations: [AppComponent],
-	imports: [
-		BrowserModule,
-		AppRoutingModule,
-		PagesModule,
-		AngularFireModule.initializeApp(environment.firebaseConfig),
-		AngularFireAuthModule
-	],
+	imports: [BrowserModule, AppRoutingModule, CoreModule],
 	providers: [],
 	bootstrap: [AppComponent]
 })
---
+++ src/app/core/libs/libs.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AuthWrapperModule } from '@core/libs/auth-wrapper/auth-wrapper.module';
+
+@NgModule({
+	declarations: [],
+	imports: [CommonModule, AuthWrapperModule]
+})
+export class LibsModule {}
