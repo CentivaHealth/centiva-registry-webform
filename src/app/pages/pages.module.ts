eb38929a7e3941b49bc586111b5dacf1db9837ff
PavloTurchyn <pavlo.t@centiva.health>
2020-06-19 11:17:54 +0300

    inited form group

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from '@pages/login/login/login.component';
 import { FormPageComponent } from '@pages/form-page/form-page.component';
+import { SharedModule } from '@shared/shared.module';
 
 @NgModule({
 	declarations: [LoginComponent, FormPageComponent],
-	imports: [CommonModule]
+	imports: [CommonModule, SharedModule]
 })
 export class PagesModule {}
