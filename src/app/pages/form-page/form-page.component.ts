8a8389ec3bee50528c8d01634d63bf73376eb393
PavloTurchyn <pavlo.t@centiva.health>
2020-06-22 18:05:05 +0300

    added validation service + added validation to form fields

--- src/app/pages/form-page/form-page.component.ts
+++ src/app/pages/form-page/form-page.component.ts
@@ -1,25 +1,49 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { AuthService } from '@core/services/auth/auth.service';
-import { FormControl, FormGroup } from '@angular/forms';
+import {
+	AbstractControl,
+	FormControl,
+	FormGroup,
+	ValidatorFn,
+	Validators
+} from '@angular/forms';
 import * as jsPDF from 'jspdf';
+import { ValidationService } from '@core/services/validation/validation.service';
 
 @Component({
 	selector: 'app-form-page',
 	templateUrl: './form-page.component.html',
 	styleUrls: ['./form-page.component.scss']
 })
-export class FormPageComponent {
-	form = new FormGroup({
-		name: new FormControl(''),
-		surName: new FormControl(''),
-		dateOfBirth: new FormControl(''),
-		testingDate: new FormControl(''),
-		testResult: new FormControl('')
-	});
+export class FormPageComponent implements OnInit {
+	form: FormGroup;
 	myAngularxQrCode = '';
 	@ViewChild('htmlData') htmlData: ElementRef;
 
-	constructor(private authService: AuthService) {}
+	constructor(
+		private authService: AuthService,
+		private validationService: ValidationService
+	) {}
+
+	ngOnInit(): void {
+		this.createForm();
+	}
+
+	createForm(): void {
+		this.form = new FormGroup({
+			name: new FormControl('', [...this.validationService.setValidators()]),
+			surName: new FormControl('', [...this.validationService.setValidators()]),
+			dateOfBirth: new FormControl('', [
+				...this.validationService.setValidators()
+			]),
+			testingDate: new FormControl('', [
+				...this.validationService.setValidators()
+			]),
+			testResult: new FormControl('', [
+				...this.validationService.setValidators()
+			])
+		});
+	}
 
 	signOut(): void {
 		this.authService.signOut();
