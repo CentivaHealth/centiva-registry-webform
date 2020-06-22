942df037ad269cd2cc9164bf3262c009d22e15dc
PavloTurchyn <pavlo.t@centiva.health>
2020-06-22 18:31:08 +0300

    added validation for login

--- src/app/pages/login/login/login.component.ts
+++ src/app/pages/login/login/login.component.ts
@@ -1,13 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from '@core/services/auth/auth.service';
+import { FormControl, FormGroup } from '@angular/forms';
+import { ValidationService } from '@core/services/validation/validation.service';
 
 @Component({
 	selector: 'app-login',
 	templateUrl: './login.component.html',
 	styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
-	constructor(private authService: AuthService) {}
+export class LoginComponent implements OnInit {
+	form: FormGroup;
+	constructor(
+		private authService: AuthService,
+		private validationService: ValidationService
+	) {}
+
+	ngOnInit(): void {
+		this.createForm();
+	}
+
+	createForm(): void {
+		this.form = new FormGroup({
+			email: new FormControl('', [
+				...this.validationService.setValidators('email')
+			]),
+			password: new FormControl('', [...this.validationService.setValidators()])
+		});
+	}
 
 	signIn(userName: string, userPassword: string): void {
 		this.authService.signIn(userName, userPassword);
