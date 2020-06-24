71d5ff6dd89678a28d4ba09fec3f2d93634cc03a
PavloTurchyn <pavlo.t@centiva.health>
2020-06-24 17:05:52 +0300

    updated libs module

--- src/app/core/libs/libs.module.ts
+++ src/app/core/libs/libs.module.ts
@@ -1,9 +1,4 @@
-import {
-	ModuleWithProviders,
-	NgModule,
-	Optional,
-	SkipSelf
-} from '@angular/core';
+import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthWrapperModule } from '@core/libs/auth-wrapper/auth-wrapper.module';
 import { QrBuilderWrapperModule } from '@core/libs/qr-builder-wrapper/qr-builder-wrapper.module';
@@ -26,17 +21,5 @@ import { DateTimePipesWrapperModule } from '@core/libs/date-time-pipes-wrapper/d
 	]
 })
 export class LibsModule {
-	constructor(@Optional() @SkipSelf() parentModule?: LibsModule) {
-		if (parentModule) {
-			throw new Error(
-				'Shared Libs module already loaded. Import in root module only.'
-			);
-		}
-	}
-
-	static forRoot(): ModuleWithProviders {
-		return {
-			ngModule: LibsModule
-		};
-	}
+	constructor() {}
 }
