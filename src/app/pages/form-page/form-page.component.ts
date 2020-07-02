72f50c48d144267553c65bf3a9ba0c201b45461e
PavloTurchyn <pavlo.t@centiva.health>
2020-07-02 13:17:38 +0300

    change test result

--- src/app/pages/form-page/form-page.component.ts
+++ src/app/pages/form-page/form-page.component.ts
@@ -153,7 +153,7 @@ export class FormPageComponent implements OnInit {
 			testDate: this.form.value.testDate,
 			testLab: this.testLabName,
 			testProvider: this.form.value.testProvider,
-			testResult: this.form.value.testResult === '1' ? 'positive' : 'negative',
+			testResult: this.form.value.testResult,
 			v: this.version
 		};
 		this.qrDataString = JSON.stringify(qrData);
