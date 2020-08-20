83d89c17f5d6d93633e40731fa2b37ed0d7ba8ba
PavloTurchyn <pavlo.t@centiva.health>
2020-08-20 11:26:13 +0300

    connect auth0

--- src/app/pages/login/login/login.component.ts
+++ src/app/pages/login/login/login.component.ts
@@ -5,6 +5,7 @@ import { ValidationService } from '@core/services/validation/validation.service'
 import { Router } from '@angular/router';
 import { MessageHandlerService } from '@core/services/message-handler/message-handler.service';
 import { filter } from 'rxjs/operators';
+import { Auth0Service } from '@core/services/auth0/auth0.service';
 
 @Component({
 	selector: 'app-login',
@@ -12,12 +13,15 @@ import { filter } from 'rxjs/operators';
 	styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+	isAuthenticated: boolean;
+
 	form: FormGroup;
 	constructor(
 		private authService: AuthService,
 		private validationService: ValidationService,
 		private router: Router,
-		private messageHandlerService: MessageHandlerService
+		private messageHandlerService: MessageHandlerService,
+		private auth0Service: Auth0Service
 	) {}
 
 	ngOnInit(): void {
@@ -35,13 +39,24 @@ export class LoginComponent implements OnInit {
 		});
 	}
 
-	signIn(userName: string, userPassword: string): void {
-		this.authService.signIn(userName, userPassword);
-		this.authService.userIsLoggedIn.subscribe(
-			(data): void => {
-				this.router.navigateByUrl('form');
-			},
-			(error): void => this.messageHandlerService.errorMessage(error.message)
-		);
+	// signIn(userName: string, userPassword: string): void {
+	// 	this.authService.signIn(userName, userPassword);
+	// 	this.authService.userIsLoggedIn.subscribe(
+	// 		(data): void => {
+	// 			this.router.navigateByUrl('form');
+	// 		},
+	// 		(error): void => this.messageHandlerService.errorMessage(error.message)
+	// 	);
+	// }
+
+	signIn(): void {
+		this.auth0Service.login('/login');
+	}
+
+	qwe() {
+		// this.isAuthenticated = this.auth0Service.loggedIn;
+		this.auth0Service.isAuthenticated$.subscribe((data) => {
+			this.isAuthenticated = data;
+		});
 	}
 }
