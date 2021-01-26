b12b7597b635bae83a1c4c388b8b4e013c96a462
ihorcherepakha <ihor.c@centiva.health>
2021-01-26 21:51:52 +0200

    add logout logic if session expired

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Auth0Service } from '@core/services/auth0/auth0.service';
 
 @Component({
-	selector: 'app-root',
-	templateUrl: './app.component.html',
-	styleUrls: ['./app.component.scss']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-	title = 'centiva-registry-webform';
+export class AppComponent implements OnInit {
+  title = 'centiva-registry-webform';
+  constructor(private auth: Auth0Service) {}
+  ngOnInit(): void {
+    const token = localStorage.getItem('idToken');
+    const parsedToken = JSON.parse(atob(token.split('.')[1]));
+    setInterval(() => {
+      const currentTime = new Date().getTime() / 1000;
+      if (currentTime > parsedToken.exp) {
+        this.auth.logout();
+      }
+    }, 10000);
+  }
 }
