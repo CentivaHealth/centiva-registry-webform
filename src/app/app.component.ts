866b242c05c3ffc1cf27e172ba43451d58f72abd
Mykola Bakai <mykola.bakay@sombrainc.com>
2021-01-29 22:48:55 +0200

    Revert token expiry fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { Auth0Service } from '@core/services/auth0/auth0.service';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  title = 'centiva-registry-webform';
-  constructor(private auth: Auth0Service) {}
-  ngOnInit(): void {
-    const token = localStorage.getItem('idToken');
-    const parsedToken = JSON.parse(atob(token.split('.')[1]));
-    setInterval(() => {
-      const currentTime = new Date().getTime() / 1000;
-      if (currentTime > parsedToken.exp) {
-        this.auth.logout();
-      }
-    }, 10000);
-  }
+export class AppComponent {
+	title = 'centiva-registry-webform';
+
+	constructor() {}
 }
